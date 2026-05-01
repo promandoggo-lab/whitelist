d90872fefa2c050644bc1495d59b49adc9787904
nazumirui5-oss <nazumirui5@gmail.com>
2026-05-01 14:19:13 +0700

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,20 @@
+local function date(d, m, y)
+    return os.time({day=d, month=m, year=y, hour=0, min=0, sec=0})
+end
+
+return {
+    -- [[ VIP PERMANENT ]]
+    [9593359594] = math.huge,
+    [10485997905] = math.huge,
+    [10365005803] = math.huge,
+
+    -- [[ VIP 1 BULAN ]]
+    -- Format: [ID] = date(Tanggal, Bulan, Tahun)
+    [9056010980] = date(1, 6, 2026), 
+    [9268364063] = date(1, 6, 2026), 
+    [8153683757] = date(1, 6, 2026), 
+    [8230713737] = date(1, 6, 2026), 
+    [10646320247] = date(1, 6, 2026), 
+    [341506349] = date(1, 6, 2026), 
+    [7899843409] = date(1, 6, 2026), 
+}
