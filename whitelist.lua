bac5aff8b8233741f9c72e52e97189a90707dfc9
nazumirui5-oss <nazumirui5@gmail.com>
2026-05-10 09:13:23 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -11,7 +11,7 @@ return {
     [8278252628] = math.huge,
     [4045325529] = math.huge,
     [8698142467] = math.huge,
-    [4714503828] = math.huge,
+    [10494185985] = math.huge,
 
     -- [[ VIP 1 BULAN ]]
     -- Format: [ID] = date(Tanggal, Bulan, Tahun)
