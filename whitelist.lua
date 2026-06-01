b2c0819659e801d1a03846990f599011a8635ee7
nazumirui5-oss <nazumirui5@gmail.com>
2026-06-02 00:00:32 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -20,7 +20,7 @@ return {
     [8698142467] = math.huge,
     [10494185985] = math.huge,
     [10832254242] = math.huge,
-    [3189967384] = math.huge,
+    [10512592607] = math.huge,
     [1375771009] = math.huge,
 
     -- [[ VIP 1 BULAN ]]
