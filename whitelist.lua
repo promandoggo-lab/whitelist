f05630be9d9fb9353ed96885f5c2217c737a2a4a
promandoggo-lab <promandoggo@gmail.com>
2026-06-11 21:05:54 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -4,7 +4,7 @@ end
 
 return {
     -- [[ OWNER / ADMIN ]]
-    [5673689347] = math.huge, 
+    [7536276329] = math.huge, 
     [9690660225] = math.huge,
     [8274108424] = math.huge,
     [7376659191] = math.huge,
